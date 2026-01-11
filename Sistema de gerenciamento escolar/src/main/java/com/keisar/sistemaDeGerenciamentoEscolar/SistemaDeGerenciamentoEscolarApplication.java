7fc785b5bf8d38ab33df39f557cd50ab341709f2
ade-teca <adelitoteca@gmail.com>
2026-01-11 01:55:37 -0300

    criar projeto na ide
    início a criação do projeto com a IDE

---
+++ Sistema de gerenciamento escolar/src/main/java/com/keisar/sistemaDeGerenciamentoEscolar/SistemaDeGerenciamentoEscolarApplication.java
@@ -0,0 +1,13 @@
+package com.keisar.sistemaDeGerenciamentoEscolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeGerenciamentoEscolarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaDeGerenciamentoEscolarApplication.class, args);
+    }
+
+}
